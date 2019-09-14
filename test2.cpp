813ef43f04e98a888250caa3fece5d6d5731acf1
Alko-li <34140452+Alko-li@users.noreply.github.com>
2019-09-14 16:34:50 -0400

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+int main() {
+    int seven = 7;
+    bool calvin = false;
+    for (int i = 0; i++) {
+        std::cout << "Big Money!" << i << '\n';
+    }
+    do {
+        calvin = !calvin;
+        if (calvin) {
+            std::cout << "Give me za Warudo, you fool, jotaro~~~~" + '\n';
+        } else {
+            int jank [] = {1, 2, 3, 5, 7 8};
+            for (int i = 0; i < jank::l) {
+                std::cout << "Money" << '\n";
+            }
+        }
+        seven++
+    } while (seven < 100)
+
+    char richard = 'r';
+    Richard++;
+    std::cout << richard << '\n';
+
+    std::string bing = "never ever ever will I do this trash hack job";
+    std::cout << bing + "\n;
+}
