e67ce9230ac4a2b81e927d84c0f0e328a56602ea
Alko-li <34140452+Alko-li@users.noreply.github.com>
2019-09-14 15:40:32 -0400

    Add files via upload
    Test file 1 for c++ code with errors

---
+++ test1.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+/*
+using namespace std;
+class User {
+    public:
+        User(string username, int password) {
+            this->username = username;
+            this->password = password;
+            numUsers++;
+            newestUser = *this;
+        }
+        public User() {
+            cout << "wowowowow"
+        }
+        static void setDisplayNewest(bool displayNewest) {
+            User::displayNewest = displayNewest;
+        }
+
+        static int getNumUsers() {
+            return User::numUsers;
+        }
+
+        void getUsername() {
+            std::string << " message " << 1999;
+        }
+        String getUsername() {
+            return this->username;
+        }
+
+        static string getWelcomeMessage() {
+            if (User::numUsers == 0) {
+                return "No user yet\n";
+            } else if (User::displayNewest){
+                return newestUser.username + " has recently joined. Welcome him\n";
+            } else {
+                return "Welcome! There" + numUsers + " people in the server\n";
+            }
+        }
+
+        void changePassword(string usernameInput, int passwordInput, int newPassword) {
+            if (validLogin(usernameInput, passwordInput)) {
+                this->password = newPassword;
+            }
+        }
+
+        boolean validLogin(string usernameInput, int passwordInput) {
+            return usernameInput == this->username && passwordInput == this->password;
+        }
+
+    private:
+        string username;
+        password;
+
+        static int numUsers
+        static User newestUser;
+        static bool displayNewest;
+};
+
+int User::numUsers = 0;
+User User::newestUser = NULL;
+bool User::displayNewest = false;
+*/
+int main() {
+    std::string cool = "wowie" << 17 << "\n";
+    //User User1(12, "1331");
+    //User tim;
+    int tim;
+    std::cout << cool;
+}
