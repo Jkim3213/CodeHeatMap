9f744cc6ba1883cfc51481c3ad906f3f39444200
Jinwoo Kim <jinwookim3213@outlook.com>
2019-09-14 21:28:13 -0400

    fixedx2

--- metrics.py
+++ metrics.py
@@ -8,8 +8,12 @@ def main(key, file_name, num_programs):
     os.system(f'rm master.txt master_master_{key}.tok')
     f = h5py.File(file_name,'r')
     print(len(f['functionSource']))
+    print(len(f['CWE-119']))
+    print(len(f['CWE-120']))
+    print(len(f['CWE-469']))
+    print(len(f['CWE-476']))
+    print(len(f['CWE-other']))
     rand_list = random.sample(range(len(f['functionSource'])), num_programs)
-    print(rand_list)
     for i in rand_list:
         program = f['functionSource'][i]
         file = open('place_holder.c', 'w')
@@ -22,16 +26,16 @@ def main(key, file_name, num_programs):
         master_text = m.read()
         master_list = master_text.splitlines()
         master_master = open(f'master_master_{key}.tok', 'ab')
-        for i, line in enumerate(master_list):
+        for i, ran in enumerate(rand_list):
+            line = master_list[i]
             int_tokens = line.split()
             int_tokens = list(map(int, int_tokens))
 
             for j in range(len(int_tokens), 500):
                 int_tokens.append(0)
             int_tokens = int_tokens[:500]
-            print(len(int_tokens))
             t_value = 0
-            if f['CWE-119'][i]:
+            if f['CWE-119'][ran] or f['CWE-120'][ran] or f['CWE-469'][ran] or f['CWE-476'][ran] or f['CWE-other'][ran]:
                 t_value = 1
             int_tokens.insert(0, t_value)
             
